2502a5392fbaa19658e11aa46828a252e4bd95aa
jfdelgad <36968968+jfdelgad@users.noreply.github.com>
2020-07-13 21:26:57 +0200

    Update DCAFactory.sol

--- contracts/DCAFactory.sol
+++ contracts/DCAFactory.sol
@@ -2,11 +2,14 @@ pragma solidity ^0.6;
 
 import 'https://github.com/ETH-Pantheon/Dollar-Cost-Averaging/blob/master/contracts/CloneFactory.sol';
 
+abstract contract DCA{
+    function setup(address owner_) virtual public returns(bool);
+}
+
 contract DCAFactory is CloneFactory{
     
     address DCATemplate = 0x276Eb817A919414EE1b67DAf3A6C29a20f61384F;
     mapping(address => address) private users;
-    
     constructor() public {
         
     }
@@ -14,6 +17,7 @@ contract DCAFactory is CloneFactory{
     
     function createDCA() public{
         address userContract = createClone(DCATemplate);
+        require(DCA(userContract).setup(msg.sender));
         users[msg.sender] = userContract;
     }
     

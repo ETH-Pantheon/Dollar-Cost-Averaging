0f44438b92c691244af50cf1e471fa23a44535b0
jfdelgad <36968968+jfdelgad@users.noreply.github.com>
2020-10-17 17:02:17 +0200

    Create factory.sol

---
+++ contracts/factory.sol
@@ -0,0 +1,27 @@
+pragma solidity >= 0.5 < 0.8;
+
+import './dcaUserAccount.sol';
+import './implementationsRegistry';
+
+contract DCAFactory{
+    address public admin;
+    implementationsRegistry private Registry;
+    event AccountCreated(address indexed owner, address account);
+    
+    constructor(address _impl) public {
+        admin = msg.sender;
+        Registry = new implementationsRegistry(_impl); 
+    }
+    
+    function registerImplementation(address _impl) public{
+        require(msg.sender==admin);
+        require(Registry.register(_impl));
+    }
+    
+    function createDCA() public{
+        dcaUserAccount userContract = new dcaUserAccount(address(Registry));
+        require(interfaceDCA(address(userContract)).setup(msg.sender));
+        emit AccountCreated(msg.sender, address(userContract));
+    }
+        
+}

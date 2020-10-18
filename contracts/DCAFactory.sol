1893738e42b57df842169216b6d52a11ad250448
jfdelgad <36968968+jfdelgad@users.noreply.github.com>
2020-10-18 12:39:02 +0200

    Update DCAFactory.sol

--- contracts/DCAFactory.sol
+++ contracts/DCAFactory.sol
@@ -2,7 +2,6 @@ pragma solidity >= 0.5 < 0.8;
 
 import "./DCAUserAccount.sol";
 import "./implementationsRegistry.sol";
-import "./IDCA.sol";
 
 contract DCAFactory{
     address public admin;
@@ -20,8 +19,8 @@ contract DCAFactory{
     }
     
     function createDCA() public{
-        dcaUserAccount userContract = new dcaUserAccount(address(Registry),admin);
-        require(IDCA(address(userContract)).setup(msg.sender));
+        dcaUserAccount userContract = new dcaUserAccount(address(Registry));
+        require(userContract.setup(msg.sender,admin));
         emit AccountCreated(msg.sender, address(userContract));
     }
     

a5e0df573e6e0f95dff9bcd8ac32d3029af264a8
jfdelgad <36968968+jfdelgad@users.noreply.github.com>
2020-10-18 12:03:25 +0200

    Update IDCA.sol

--- contracts/IDCA.sol
+++ contracts/IDCA.sol
@@ -1,5 +1,5 @@
 pragma solidity >= 0.5 < 0.8;
 
 interface IDCA{
-    function setup(address owner_) external returns(bool);
+    function setup(address owner_, address creator) external returns(bool);
 }
